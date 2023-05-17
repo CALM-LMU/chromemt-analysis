f9c534a16965bcb9dc1616d65177eb013787269d
David Hoerl <hoerldavid@gmail.com>
2023-05-17 10:50:42 +0200

    small changes, remove some outputs

--- chromemt_analysis.py
+++ chromemt_analysis.py
@@ -4,7 +4,7 @@ from io import TextIOWrapper
 import numpy as np
 from skimage.filters import threshold_li
 from skimage.exposure import equalize_adapthist
-from skimage.morphology import remove_small_objects, ball, binary_erosion
+from skimage.morphology import remove_small_objects, binary_erosion
 from scipy.ndimage import median_filter
 from scipy.ndimage import generate_binary_structure
 from scipy.ndimage import distance_transform_edt
@@ -31,6 +31,15 @@ def disk_centered(radius, dtype=np.uint8):
     L = np.arange(-radius_ceil, radius_ceil + 1)
     X, Y = np.meshgrid(L, L)
     return np.array((X ** 2 + Y ** 2) <= radius ** 2, dtype=dtype)
+
+def hypersphere_centered(rank, radius, dtype=np.uint8):
+    '''
+    rank-n hypersphere, should be just like disk/ball but also provide reasonable results for non-integer radii
+    '''
+    radius_ceil = int(np.ceil(radius))
+    L = np.arange(-radius_ceil, radius_ceil + 1)
+    dist = np.linalg.norm(np.stack(np.meshgrid(*[L]*rank, indexing='ij')), axis=0)
+    return (dist<=radius).astype(dtype)
 
 def parse_simulation_dat(file):
 
@@ -63,8 +72,9 @@ def parse_simulation(fd):
     psz, shape = lines[0].split()
     psz, shape = float(psz), int(shape)
 
-    # new files have second line containing a single number, skip
-    # TODO: what is this?
+    # new files have second line containing a single number (and no whitespace-separated pixels)
+    # the number corresponds to the number of points used to approximate the structure
+    # and is currently not used in the image-based analyses, so we skip it if it is present
     if not ' ' in lines[1]:
         remaining_lines = lines[2:]
     else:
@@ -83,19 +93,20 @@ def fwhm2sigma(fwhm):
 
 def segment_like_paper(patch, clahe_size=78, min_object_size=500, radius=2, planewise_median=True, planewise_clahe=True, imagej_outlier_bright=True, imagej_outlier_dark=True):
     '''
-    Segmentation pipeline similar to ChromEMT pipeline
-    clahe_size is in pixels, 78 ~ 100nm at 1.28nm pixel size
+    Segmentation pipeline similar to ImageJ-based ChromEMT pipeline
+    clahe_size is in pixels, default 78px ~ 100nm at 1.28nm pixel size (like in paper)
     '''
 
-    # normailzed clip limit? TODO: is this equivalent to 3 in ImageJ?
+    # normailzed clip limit, should be equivalent to default slope=3 in ImageJ
     cl = 3 / 256
 
     # 1) CLAHE
+    # NOTE: ImageJ does CLAHE planewise
     if planewise_clahe:
         patch_eq = np.stack([equalize_adapthist(patch_i, clahe_size, cl) for patch_i in patch])
     else:
         patch_eq = equalize_adapthist(patch, clahe_size, cl)
-    
+
     # 2) Li thresholding
     mask = patch_eq < threshold_li(patch_eq)
 
@@ -110,7 +121,7 @@ def segment_like_paper(patch, clahe_size=78, min_object_size=500, radius=2, plan
             if imagej_outlier_dark:
                 mask_i &= median_filter(mask_i, footprint=disk_centered(radius+0.5))
     else:
-        mask = median_filter(mask, footprint=ball(radius))
+        mask = median_filter(mask, footprint=hypersphere_centered(3, radius))
 
     # 4) remove small objects should correspond to size threshold in 3D Object Counter
     mask = remove_small_objects(mask, min_object_size)
@@ -149,7 +160,7 @@ def continuous_erosion(mask, erosion_radius=None):
         erosion_radius = np.arange(1, mask.shape[0]//2)
 
     # TODO: slow, parallelize or iterative erosion with iterative application of small sphere
-    eroded = np.stack([binary_erosion(mask, ball(r)) for r in erosion_radius])
+    eroded = np.stack([binary_erosion(mask, hypersphere_centered(3, r)) for r in erosion_radius])
 
     return eroded.sum(axis=(1,2,3)) / mask.sum()
 
@@ -187,7 +198,7 @@ def continuous_erosion_edt(mask, erosion_radius=None):
 
 def linear_fit_to_residual_volume2(residual_volume, erosion_radius=None, n_first_values_to_include=5):
     '''
-    Fit linear model erosion_radius ~ residual volume to residual volumes ( x ~ y )
+    Fit linear model residual_volume ~ erosion_radius to residual volumes ( x ~ y )
     Returns
     =======
     estimated_diameter: float
@@ -237,9 +248,3 @@ def linear_fit_to_residual_volume(residual_volume, erosion_radius=None, n_first_
     radius_estimated =  - lm.intercept_ / lm.coef_[0]
 
     return radius_estimated * 2, lm
-
-def hypersphere(rank, radius):
-    '''
-    rank-n hypersphere, should be just like disk/ball
-    '''
-    return np.linalg.norm(np.stack(np.meshgrid(*[np.arange(-radius, radius+1)]*rank, indexing='ij')), axis=0)<=radius
